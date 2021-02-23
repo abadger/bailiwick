38e946ce97fd7b9a1b279e3af3257a2f100b492d
Toshio Kuratomi <a.badger@gmail.com>
2021-02-23 11:55:46 -0800

    Implement some context functions.
    * create_context() creates a new context object
    * get_context() retrieves an existing context object

---
+++ bailiwick/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+# Author: Toshio Kuratomi <a.badger@gmail.com>
+# License: LGPLv3+
+# Copyright: Toshio Kuratomi, 2021
+
+from .context import create_context, get_context
--- bailiwick/errors.py
+++ bailiwick/errors.py
@@ -1,2 +1,11 @@
-class MyException(Exception):
-    pass
+# coding: utf-8
+# Author: Toshio Kuratomi <a.badger@gmail.com>
+# License: LGPLv3+
+# Copyright: Toshio Kuratomi, 2021
+
+class DuplicateContext(Exception):
+    """Tried to create a context that already exists."""
+
+
+class MustBeFrozen(Exception):
+    """An operation on a :class:`bailiwick.context.ContextDict` requires it to be frozen."""
